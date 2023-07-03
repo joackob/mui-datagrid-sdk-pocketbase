73e9bb925dc25dbd6dd9a4f2a23046f260adc132
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-07-03 17:52:39 -0300

    test implemented

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,19 +1,19 @@
 // __tests__/index.test.js
 
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Home from "@/pages/index";
 import App from "@/src/app/App";
 import "@testing-library/jest-dom";
 
 describe("Home", () => {
-  test("renders a heading", () => {
+  test("renders a heading", async () => {
     render(
       <App>
         <Home />
       </App>
     );
 
-    const icon = screen.getByText("DG");
+    const icon = await waitFor(() => screen.getByText("DG"));
     expect(icon).toBeVisible();
   });
 });
